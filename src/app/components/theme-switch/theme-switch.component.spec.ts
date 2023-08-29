2f7ade051f18d5c025997bae9982e6646f58caf4
Rodrigo Bournissent <rodrigo@gramble.com>
2023-08-29 17:05:14 +0200

    Fix ThemeSwitchComponent tests

--- src/app/components/theme-switch/theme-switch.component.spec.ts
+++ src/app/components/theme-switch/theme-switch.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatIconModule } from '@angular/material/icon';
+
 import { ThemeSwitchComponent } from './theme-switch.component';
 
 describe('ThemeSwitchComponent', () => {
@@ -8,6 +10,7 @@ describe('ThemeSwitchComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [MatIconModule],
       declarations: [ThemeSwitchComponent]
     });
     fixture = TestBed.createComponent(ThemeSwitchComponent);
