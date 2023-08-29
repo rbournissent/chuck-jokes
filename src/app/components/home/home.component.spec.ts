8376087664b1b3ffe1dc82533f8b9fefbd723fd3
Rodrigo Bournissent <rodrigo@gramble.com>
2023-08-29 16:07:02 +0200

    Fix HomeComponent tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { HomeComponent } from './home.component';
 
@@ -10,7 +12,10 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [MatProgressBarModule],
+      imports: [
+        MatProgressBarModule,
+        MatButtonModule
+      ],
       declarations: [HomeComponent]
     });
     fixture = TestBed.createComponent(HomeComponent);
