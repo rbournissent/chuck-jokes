44b5e11544026951d40e3ecbfe4c5e49029e0b85
Rodrigo Bournissent <rodrigo@gramble.com>
2023-08-29 13:03:17 +0200

    Fix AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -7,7 +11,11 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      imports: [RouterTestingModule],
+      declarations: [
+        AppComponent,
+        NavbarComponent
+      ]
     });
 
     fixture = TestBed.createComponent(AppComponent);
