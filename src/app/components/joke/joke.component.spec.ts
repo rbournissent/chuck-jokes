8f33ddac7100fb52c1f9a319ed981297b298fa82
Rodrigo Bournissent <rodrigo@gramble.com>
2023-08-29 17:45:03 +0200

    JokeComponent: Test toggleFavorite method

--- src/app/components/joke/joke.component.spec.ts
+++ src/app/components/joke/joke.component.spec.ts
@@ -4,10 +4,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
 import { JokeComponent } from './joke.component';
+import { Joke } from 'src/app/models/joke';
 
 describe('JokeComponent', () => {
   let component: JokeComponent;
   let fixture: ComponentFixture<JokeComponent>;
+  const joke: Joke = {
+    id: '1',
+    text: 'Laugh, please!'
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,10 +24,27 @@ describe('JokeComponent', () => {
     });
     fixture = TestBed.createComponent(JokeComponent);
     component = fixture.componentInstance;
+    component.joke = joke;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a joke defined', () => {
+    expect(component.joke).toBeDefined();
+    expect(component.joke).toEqual(joke);
+  });
+
+  it('should toggle joke as favorite', () => {
+    expect(component.joke?.isFavorite).toBeUndefined();
+
+    component.toggleFavorite();
+    expect(component.joke?.isFavorite).toBeTrue();
+
+    component.toggleFavorite();
+    expect(component.joke?.isFavorite).toBeFalse();
   });
 });
